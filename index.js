6874459550a3fc317644bc9b91e880d7b76f58be
ANI-RUDH <117719175+ANIi-RUDH@users.noreply.github.com>
2023-08-05 21:15:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+import superheroes from"superheroes";// instead od this we could have done  var generateName=require("sillyname");  
+var shero=superheroes.random();
+console.log("I am---> "+ shero);
